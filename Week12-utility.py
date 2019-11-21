f81ec1d72527a15647d5c4ba678bbc8ee463acb8
rose45672 <ash.rrez@gmail.com>
2019-11-21 10:15:41 -0700

    added FindWordCount

--- Week12-utility.py
+++ Week12-utility.py
@@ -8,9 +8,18 @@ def LoadFile(filename):
         contents = f.read()
     contents = contents.split('\n')
     return contents
+
 # UpdateString
 def UpdateString(string_one, string_two, num):
     list_one = list(string_one)
     list_one[num] = string_two
     new_string = ''.join(list_one)
     return new_string 
+
+# FindWordCount
+def FindWordCount(user_list, user_string):
+    count = 0
+    for n in user_list:
+        count += n.count(user_string)
+    return count
+
