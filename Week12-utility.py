4123173fdca2ebc6b9745c04a68555aa5ab3d3e1
rose45672 <ash.rrez@gmail.com>
2019-11-21 09:48:25 -0700

    added UpdateString

--- Week12-utility.py
+++ Week12-utility.py
@@ -3,3 +3,14 @@ def PrintOutput(input_string):
     print("OUTPUT", input_string)
     
 # LoadFile
+def LoadFile(filename):
+    with open(filename, 'r') as f:
+        contents = f.read()
+    contents = contents.split('\n')
+    return contents
+# UpdateString
+def UpdateString(string_one, string_two, num):
+    list_one = list(string_one)
+    list_one[num] = string_two
+    new_string = ''.join(list_one)
+    return new_string 
